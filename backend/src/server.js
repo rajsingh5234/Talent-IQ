7d7c32f86b9544ec60f2d3f2b80610f970356b0b
rajsingh51 <raj@turnsapp.com>
2025-11-02 14:18:50 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.get('/health', (req, res) => {
     res.status(200).json({ msg: 'api is up and running' });
 });
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running on http://localhost:${ENV.PORT}`);
