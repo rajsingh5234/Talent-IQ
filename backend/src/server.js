c91c02d50102659d4fbfc22221617c2d53210817
rajsingh51 <raj@turnsapp.com>
2025-11-23 16:53:01 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import { serve } from 'inngest/express';
 import { inngest, functions } from './lib/inngest.js';
+import { clerkMiddleware } from '@clerk/express';
+import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 
@@ -12,8 +14,10 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds a auth field in  req: req.auth()
 
-app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('api/chat', chatRoutes);
 
 app.get('/health', (req, res) => {
     res.status(200).json({ msg: 'api is up and running' });
