bcd9c32e3ab63e10b9b9e0b8b52ca3207f5750cb
rajsingh51 <raj@turnsapp.com>
2025-11-24 12:39:30 +0530

    session route and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { serve } from 'inngest/express';
 import { inngest, functions } from './lib/inngest.js';
 import { clerkMiddleware } from '@clerk/express';
 import chatRoutes from './routes/chatRoutes.js';
+import sessionRoutes from './routes/sessionRoutes.js';
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(clerkMiddleware()); // this adds a auth field in  req: req.auth()
 
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('api/chat', chatRoutes);
+app.use('api/sessions', sessionRoutes);
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'api is up and running' });
