5bc725e1100945a6e2815584178e6da6ddc2bef2
rajsingh51 <raj@turnsapp.com>
2025-11-24 14:42:16 +0530

    landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import { useUser } from '@clerk/clerk-react'
 import { Toaster } from 'react-hot-toast'
+import DashboardPage from './pages/DashboardPage'
 
 const App = () => {
 
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return null; // or a loading spinner
+  }
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />} />
+        <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} />
         <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} />
       </Routes>
 
