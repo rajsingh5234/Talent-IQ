60f5e621101c08fa8189a496c0f3914898f1de28
rajsingh51 <raj@turnsapp.com>
2025-11-24 20:56:24 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ProblemsPage from './pages/ProblemsPage'
 import { useUser } from '@clerk/clerk-react'
 import { Toaster } from 'react-hot-toast'
 import DashboardPage from './pages/DashboardPage'
+import ProblemPage from './pages/ProblemPage'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />} />
         <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} />
         <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} />
+        <Route path='/problem/:id' element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />} />
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
